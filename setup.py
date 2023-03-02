61d8397aee5fcb9f5fd56802bda459fb538e6a36
robert <robertgsinger@gmail.com>
2023-03-02 09:40:31 -0600

    Updates release notes

--- setup.py
+++ setup.py
@@ -17,15 +17,16 @@ classifiers = [
     "Framework :: Django :: 3.0",
     "Framework :: Django :: 3.1",
     "Framework :: Django :: 3.2",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
 ]
 setup(
     name="drf-access-policy",
-    version="1.4.0",
+    version="1.5.0",
     description="Declarative access policies/permissions modeled after AWS' IAM policies.",
     author="Robert Singer",
     author_email="robertgsinger@gmail.com",
